599eca9661781ee9fd7d7cc00625a4703ca71332
kh8514 <47620827+kh8514@users.noreply.github.com>
2022-06-11 14:03:19 +0900

    morgan로그관리

--- app/app.js
+++ app/app.js
@@ -3,10 +3,17 @@
 //module
 const express = require("express")
 const bodyParser = require("body-parser")
-const app = express()
+const morgan = require("morgan")
 const dotenv = require("dotenv")
+const fs = require("fs")
 dotenv.config()
 
+const app = express()
+
+const accessLogStream = fs.createWriteStream(
+    `${__dirname}/log/access.log`,
+    { flags: 'a'}
+) 
 
 // app setting
 app.set("views", "./src/views")
@@ -16,6 +23,7 @@ app.use(bodyParser.json())
 // URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우
 // 제대로 인식되지 않는 문제 해결
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(morgan("dev", {stream: accessLogStream}))
 
 
 // routing
